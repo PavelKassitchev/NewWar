981579e8712bb83318796e052cbd03aab4a390ae
paul <pavkascool@gmail.com>
2019-06-19 09:35:56 +0300

    from office: font.getData().setSize added

--- core/src/com/pavka/MileStone.java
+++ core/src/com/pavka/MileStone.java
@@ -11,6 +11,8 @@ public class MileStone extends Image {
     int days = 16;
 
     public void draw(Batch batch, float alpha) {
+
+        font.getData().setScale(0.8f);
         font.draw(batch, String.valueOf(days), 60, 60);
     }
 
