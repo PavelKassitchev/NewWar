97e0245702303a6f8bf1a3986a6001a579c17cbb
pavel <pavkascool@gmail.com>
2019-11-14 18:07:07 +0300

    from transport: work with Tableau and Label

--- core/src/com/pavka/Tableau.java
+++ core/src/com/pavka/Tableau.java
@@ -1,10 +1,12 @@
 package com.pavka;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -32,16 +34,21 @@ public class Tableau extends Table {
         BitmapFont font = new BitmapFont();
         font.getData().setScale(0.7f);
         Label.LabelStyle style = new Label.LabelStyle(font, new Color(1, 0, 0, 1));
-
+        Pixmap labelColor = new Pixmap(1, 1, Pixmap.Format.RGB888);
+        labelColor.setColor(Color.GOLD);
+        labelColor.fill();
+        style.background = new Image(new Texture(labelColor)).getDrawable();
         hexLabel = new Label("Cost: " + hex.cell.getTile().getProperties().get("cost") + " Crops: " + hex.currentHarvest,
                 style);
+        //labelColor.setColor(Color.GOLD);
         //costLabel.setWidth(18);
         hexLabel.setWrap(true);
         play.addActor(hexLabel);
         add(hexLabel).width(140f);
-        hexLabel.setAlignment(0);
+        //hexLabel.setAlignment(1);
         hexLabel.setDebug(true);
         totalHeight += hexLabel.getPrefHeight();
+        labelColor.dispose();
         row();
 
         if (forces != null && forces.size > 0) {
@@ -51,7 +58,9 @@ public class Tableau extends Table {
                 forceLabel.setWrap(true);
                 play.addActor(forceLabel);
                 add(forceLabel).width(140f);
-                forceLabel.setAlignment(0);
+                forceLabel.pack();
+                forceLabel.setWidth(140f);
+                //forceLabel.setAlignment(0);
                 forceLabel.setDebug(true);
                 totalHeight += forceLabel.getPrefHeight();
                 row();
@@ -60,16 +69,22 @@ public class Tableau extends Table {
         if (base != null) {
             Label baseLabel = new Label(base.getGeneralInfo(), new Label.LabelStyle(font, new Color(1, 0, 0, 1)));
             baseLabel.setWrap(true);
+
             play.addActor(baseLabel);
             add(baseLabel).width(140f);
+            baseLabel.pack();
+            baseLabel.setWidth(140f);
             baseLabel.setDebug(true);
             totalHeight += baseLabel.getPrefHeight();
+            System.out.println(baseLabel.getHeight());
         }
-        System.out.println("Height is " + totalHeight);
+
+        System.out.println("Height is " + totalHeight + " Rows: " + getRow(totalHeight));
         setPosition(hex.getX(), hex.getY());
         setBounds(getX() - 8, getY() - 8, 164, totalHeight);
         setTouchable(Touchable.enabled);
         setVisible(true);
+        align(1);
         //setColor(0, 0, 0, 1);
         Skin skin = new Skin();
         Color color = new Color(0, 0, 0, 1);
